595148ebc5471c044c17e85f6c52c6203d24568c
Paulo J. Matos <paulojomatos@gmail.com>
2017-01-20 04:03:32 -0200

    game exported

---
+++ offline.js
@@ -0,0 +1,19 @@
+﻿{
+	"version": 1484892175,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"images/bg-sheet0.png",
+		"images/ground-sheet0.png",
+		"images/player-sheet0.png",
+		"images/wallleft-sheet0.png",
+		"images/shuriken-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
